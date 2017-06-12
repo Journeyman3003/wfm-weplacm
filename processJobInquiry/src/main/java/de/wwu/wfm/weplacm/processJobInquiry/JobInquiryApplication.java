d718b32cb8bd2f588f1f086628f77666748025b4
Tobias <toby.mai@web.de>
2017-06-12 14:46:54 +0200

    initial commit

---
+++ processJobInquiry/src/main/java/de/wwu/wfm/weplacm/processJobInquiry/JobInquiryApplication.java
@@ -0,0 +1,10 @@
+package de.wwu.wfm.weplacm.processJobInquiry;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("Loan Approval App")
+public class JobInquiryApplication {
+	// empty implementation
+}
+
