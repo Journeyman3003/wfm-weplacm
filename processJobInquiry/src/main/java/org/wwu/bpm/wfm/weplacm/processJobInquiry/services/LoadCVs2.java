880315a501bd4f50bf03f5f611037986db416b44
Tobias <toby.mai@web.de>
2017-07-09 14:21:46 +0200

    added missing java delegate classes, updated bpmn

--- processJobInquiry/src/main/java/org/wwu/bpm/wfm/weplacm/processJobInquiry/services/LoadCVs2.java
+++ processJobInquiry/src/main/java/org/wwu/bpm/wfm/weplacm/processJobInquiry/services/LoadCVs2.java
@@ -1,18 +1,17 @@
 package org.wwu.bpm.wfm.weplacm.processJobInquiry.services;
 
 import java.util.logging.Logger;
+
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 
-public class CVAvailabilityCheck implements JavaDelegate{
+public class LoadCVs2 implements JavaDelegate{
 
 	private final static Logger LOGGER = Logger.getLogger("JOB-INQUIRY");
 
 	public void execute(DelegateExecution execution) throws Exception {
-	  LOGGER.info("Processing request by '" + execution.getVariable("customerId") + "'...");
-	  
-	  execution.setVariable("sufficient", true);
-	  
+		LOGGER.info("Processing request by '" + execution.getVariable("customerId") + "'...");
+	 	  
 	}	
 	
 }
