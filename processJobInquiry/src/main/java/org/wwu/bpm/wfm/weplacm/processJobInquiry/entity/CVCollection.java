71b427bdcafbeeffe67dc5ae37d56f288dfd677a
Tobias <toby.mai@web.de>
2017-07-15 19:40:46 +0200

    modified html files

--- processJobInquiry/src/main/java/org/wwu/bpm/wfm/weplacm/processJobInquiry/entity/CVCollection.java
+++ processJobInquiry/src/main/java/org/wwu/bpm/wfm/weplacm/processJobInquiry/entity/CVCollection.java
@@ -1,5 +1,19 @@
 package org.wwu.bpm.wfm.weplacm.processJobInquiry.entity;
 
+import java.util.ArrayList;
+
 public class CVCollection {
+	private ArrayList<CV> cvs;
 	
+	public CVCollection() {
+		this.cvs = new ArrayList<CV>();
+	}
+
+	public ArrayList<CV> getCvs() {
+		return cvs;
+	}
+
+	public void setCvs(ArrayList<CV> cvs) {
+		this.cvs = cvs;
+	}
 }
