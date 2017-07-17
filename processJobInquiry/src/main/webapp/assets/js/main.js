bf19141705b254d02eb4c9eca581cbacd6341c4e
Michael Kurtz <s_kurt02@uni-muenster.de>
2017-07-17 18:13:49 +0200

    Add CV template to panel body

--- processJobInquiry/src/main/webapp/assets/js/main.js
+++ processJobInquiry/src/main/webapp/assets/js/main.js
@@ -6,8 +6,8 @@ $(document).ready(function() {
 		
 		$('#cV-panels .panel-heading').removeClass('act');
 		$(this).addClass('act');
-		nextPanelBody.slideToggle('fast');
-		$('#cV-panels .panel-body').not(nextPanelBody).slideUp('fast');
+		nextPanelBody.slideToggle(300);
+		$('#cV-panels .panel-body').not(nextPanelBody).slideUp(300);
 		
 	});
 	
@@ -16,7 +16,7 @@ $(document).ready(function() {
 		var nextPanelBody = $(this).next('.panel-body');
 
 		$(this).toggleClass('act');
-		nextPanelBody.slideToggle('fast');
+		nextPanelBody.slideToggle(300);
 		
 	});
 
